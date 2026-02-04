825938d56e5bd3fac0635bb4842c4dbef395a305
Tanmaya <tanmayanaik2020@gmail.com>
2026-02-04 11:56:03 +0530

    Added the optimal; code for the reverse code problem

--- String/Reverse.cpp
+++ String/Reverse.cpp
@@ -1,44 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// APPROACH BRUTE FORCE FIRST ITERATE AND STORE ALL THE WORD OF THE STRING IN A VECTOR THEN REVERSE THE VECTOR AND ITERATE THROUGH VECTOR AND STORE THE STRING IN RESULT
 
-//APPROACH BRUTE FORCE FIRST ITERATE AND STORE ALL THE WORD OF THE STRING IN A VECTOR THEN REVERSE THE VECTOR AND ITERATE THROUGH VECTOR AND STORE THE STRING IN RESULT
-
-string  reverseSTR(string &str){
+string reverseSTR(string &str)
+{
     vector<string> words;
-    string word="";
+    string word = "";
 
-    for(int i=0;i<str.size();i++){
-        if(str[i]!=' '){
-            word+=str[i];
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] != ' ')
+        {
+            word += str[i];
         }
-        else if(!word.empty()){
+        else if (!word.empty())
+        {
             words.push_back(word);
-            word="";
+            word = "";
         }
     }
 
-    if(!word.empty()){ //last word
+    if (!word.empty())
+    { // last word
         words.push_back(word);
     }
 
-    reverse(words.begin(),words.end());
-    string result="";
+    reverse(words.begin(), words.end());
+    string result = "";
 
-    for(int i=0;i<words.size();i++){
-        result+=words[i];
-        if(i<words.size()-1) result+=" ";
+    for (int i = 0; i < words.size(); i++)
+    {
+        result += words[i];
+        if (i < words.size() - 1)
+            result += " ";
     }
 
     return result;
-
-
 }
 
-int main(){
-    string str="   i am tanmaya and who are u";
-    string str2=reverseSTR(str);
-    cout<<str2<<endl;
-    return 0;
+string optimalReverse(string &str)
+{
+    string result = "";
+    int i = str.size() - 1;
+    while (i >= 0)
+    {
+        while (i >= 0 && str[i] == ' ')
+        {
+            i--;
+        }
+        if (i < 0)
+            break;
 
+        int end = i;
+
+        while (i >= 0 && str[i] != ' ')
+        {
+            i--;
+        } // when encounter a non space character
+
+        // lets extract the string from i to end-i
+        string word = "";
+        word += str.substr(i + 1, end - i); // this give that word
+
+        if (!word.empty())
+{
+    if (!result.empty())
+        result += " ";
+    result += word;
+}
+
+    }
+    return result;
+}
+
+int main()
+{
+    string str = "   he is  ";
+    string str2 = reverseSTR(str);
+    cout << str2 << endl;
+    cout<<str2.size()<<endl;
+    
+    string str3 = optimalReverse(str);
+    cout << str3 << endl;
+    cout<<str3.size();
+
+    return 0;
 }
