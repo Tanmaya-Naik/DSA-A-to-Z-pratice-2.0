b0a0a49fe4da4bffaf2d171c35a01f10441f80c0
Tanmaya <tanmayanaik2020@gmail.com>
2026-02-16 17:45:10 +0530

    Added the Bit manipulation code

---
+++ Bit_Manipulation/Set_ith_bit.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the number u want to find ith bit" << endl;
+    int n;
+    cin >> n;
+
+    cout << "Enter the ith bit" << endl;
+    int k;
+    cin >> k;
+
+    int bitmask=1<<k;
+    int newN=(n|bitmask);
+    cout <<"After setting the ith bit the new Value is"<< newN << endl;
+
+    return 0;
+}
